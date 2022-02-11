fb334a69f5faf9c67c6ab04e5bb5a7c88b2acb94
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-02-11 13:11:44 +0200

    ✨: add constructors

---
+++ Labs/Lab1/Perceptron.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab1
+{
+	public class Perceptron
+	{
+		public double[,] Input { get; }
+		public double[] Weights { get; }
+		public double[] Output { get; }
+
+		private readonly Random random = new();
+
+		public Perceptron(double[,] input)
+		{
+			Input = input;
+			Output = new double[input.GetLength(0)];
+		}
+
+		public Perceptron(double[,] input, double[] weights) : this(input)
+		{
+			Weights = weights;
+		}
+
+	}
+}
